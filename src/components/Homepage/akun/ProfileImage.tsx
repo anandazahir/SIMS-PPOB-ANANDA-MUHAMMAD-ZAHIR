6a562cbc17071fdebfbaa9cafbf8a4eb6c054c32
Ananda Muhammad Zahir <zhohir22@gmail.com>
2025-02-16 18:12:25 +0700

    fixing preview image

--- src/components/Homepage/akun/ProfileImage.tsx
+++ src/components/Homepage/akun/ProfileImage.tsx
@@ -31,10 +31,13 @@ export function ProfileImage({
       <div className="w-32 h-32 rounded-full overflow-hidden">
         <Image
           src={
-            previewImage || (profileImage && !profileImage.includes("null"))
-              ? profileImage
-              : "/Website Assets/Profile Photo.png"
+            previewImage
+              ? previewImage
+              : profileImage && !profileImage.includes("null")
+                ? profileImage
+                : "/Website Assets/Profile Photo.png"
           }
+          fixi
           alt="Profile"
           width={128}
           height={128}
